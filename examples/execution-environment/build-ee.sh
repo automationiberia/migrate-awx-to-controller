c6aa9a9dfb4c7233d48940db670171c2ce9bde18
pgoku <pgoku@pgoku.bcnconsulting.com>
2025-03-05 11:27:26 +0100

    pgoku satellite registry

--- examples/execution-environment/build-ee.sh
+++ examples/execution-environment/build-ee.sh
@@ -5,7 +5,7 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
-AAH=quay.io/automationiberia/aap
+AAH=satellite.bcnconsulting.com/red_ribbon/application_images/aap
 
-ansible-builder build -v3 --tag ${AAH}/${1}
-podman push ${AAH}/${1} --tls-verify=false
+ansible-builder build -v3 --tag ${AAH}_${1}
+podman push ${AAH}_${1} --tls-verify=false
